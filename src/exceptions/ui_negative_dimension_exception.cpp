3d50e7a0b03ca38a6a5bcaca4b0fccb4e238a115
Stefan Denciu <stefandenciu83@gmail.com>
2026-05-17 21:55:09 +0300

    github actions 10 test maybe unused

--- src/exceptions/ui_negative_dimension_exception.cpp
+++ src/exceptions/ui_negative_dimension_exception.cpp
@@ -4,6 +4,7 @@
 
 #include "../../include/ui_negative_dimension_exception.h"
 
+[[maybe_unused]]
 const char *ui_negative_dimension_exception::what() const noexcept {
     return "error:negative dimension for ui";
 }
